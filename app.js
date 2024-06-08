d831bb19ccb4a88c86572f94bf99c2316f0681a7
DarxyDev <damongarvin@gmail.com>
2024-06-08 01:18:04 -0400

    setup mongoose

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-//start server
-app.listen(8080);
-//set ejs
+//connect to mongodb and start server
+const connectString = process.env.MONGO_CONNECTION;
+mongoose.connect(connectString).then((result) => {
+    console.log('connected to DB');
+    app.listen(8080);
+}).catch((err) => console.log(err))
+
+//set ejs view engine
 app.set('view engine', 'ejs');
 
 //get requests
@@ -17,6 +23,4 @@ app.use((req, res) => {
     res.status(404).render('404', {
         title: '404 - Page not found'
     })
-})
-
-console.log(process.env.USER_ID)
+});
