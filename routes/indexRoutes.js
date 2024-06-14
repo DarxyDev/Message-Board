0bccf718f4f8f4319d7ae0ad12382e2686b15051
DarxyDev <damongarvin@gmail.com>
2024-06-14 19:55:47 -0400

    formatting

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const indexController = require('../controllers/indexController');
 
 router.get('/', indexController.index_get);
+
 router.get('/new', indexController.new_get);
-router.post('/new', indexController.new_post)
+router.post('/new', indexController.new_post);
 
 module.exports = router;
